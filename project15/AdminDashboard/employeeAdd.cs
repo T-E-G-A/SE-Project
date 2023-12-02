95f0d3380556a0aceca1bbb93c1aa47684025afd
Tadas Stucinskas <banginq2@gmail.com>
2023-12-02 20:37:08 +0000

    bugged update button in employeeAdd page, will attemp fix later

--- project15/AdminDashboard/employeeAdd.cs
+++ project15/AdminDashboard/employeeAdd.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AdminDashboard
 {
@@ -37,6 +38,24 @@ namespace AdminDashboard
 
         }
 
+       private void button1_Click_1(object sender, EventArgs e)
+        {
+            /* tried making this update db but not working, will mess around later unless someone wants to fix
+             
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\tadas\\Source\\Repos\\SE-Project34e\\project15\\AdminDashboard\\Database21.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into employee database values (@Id,:employeeID,@employeeFirstName,@employeeLastName,@dateOfBirth,@role)", con);
+            cmd.Parameters.AddWithValue("@Id", int.Parse(idTextBox.Text));
+            cmd.Parameters.AddWithValue("@employeeID", int.Parse(idTextBox.Text));
+            cmd.Parameters.AddWithValue("@employeeFirstName",idTextBox.Text);
+            cmd.Parameters.AddWithValue("@employeeLastName", idTextBox.Text);
+            cmd.Parameters.AddWithValue("@dateOfBirth", idTextBox.Text);
+            cmd.Parameters.AddWithValue("@role", idTextBox.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Success!");
 
+            */
+        }
     }
 }
