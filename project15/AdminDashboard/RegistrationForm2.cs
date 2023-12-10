6901666fe2804a7fbbd551aac380cf14c045cf66
T-E-G-A <104275659+T-E-G-A@users.noreply.github.com>
2023-12-10 23:27:09 +0100

    finished up registration page

--- project15/AdminDashboard/RegistrationForm2.cs
+++ project15/AdminDashboard/RegistrationForm2.cs
@@ -46,6 +46,51 @@ namespace WindowsFormsApp2
                 userName.Focus();
                 return;
             }
+            if (string.IsNullOrEmpty(passWord.Text))
+            {
+                MessageBox.Show("Enter Password", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                passWord.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(eMail.Text))
+            {
+                MessageBox.Show("Enter Email", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                eMail.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(confirmPassword.Text))
+            {
+                MessageBox.Show("Enter confirm password", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                confirmPassword.Focus();
+                return;
+            }
+            if (userName.Text.Length > 16)
+            {
+                userName.Text = userName.Text.Substring(0, 16);
+                MessageBox.Show("The text entered exceeds the maximum limit of 16 characters.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                userName.Focus();
+                return;
+            }
+            if (eMail.Text.Length > 16)
+            {
+                eMail.Text = eMail.Text.Substring(0, 16);
+                MessageBox.Show("The text entered exceeds the maximum limit of 16 characters.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                eMail.Focus();
+                return;
+            }
+            if (passWord.Text.Length > 16)
+            {
+                passWord.Text = passWord.Text.Substring(0, 16);
+                MessageBox.Show("The text entered exceeds the maximum limit of 16 characters.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                passWord.Focus();
+                return;
+            }
+            if (confirmPassword.Text.Length > 16)
+            {
+                confirmPassword.Text = confirmPassword.Text.Substring(0, 16);
+                MessageBox.Show("The text entered exceeds the maximum limit of 16 characters.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            confirmPassword.Focus();
+                return;}
             if (passWord.Text != confirmPassword.Text)
             {
                 MessageBox.Show("Passwords don't match", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -57,6 +102,7 @@ namespace WindowsFormsApp2
                 MessageBox.Show("Registration Successful", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
             else
                 MessageBox.Show("Registration Failed, Please try again", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
             mainForm f1 = new mainForm();
             f1.Show();
             this.Hide();
